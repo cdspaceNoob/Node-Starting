d91a0c03803ad437208e10b35b96fb3512a45acc
voyager <cdspacenoob@gmail.com>
2023-03-29 17:30:31 +0900

    export and import file

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    if (url === '/') {
-        response.write('<html>');
-        response.write('<head><title>My First Page</title></head>');
-        response.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
-        response.write('</html>');
+const routes = require('./routes');
 
-        return response.end();
-    }
-    if (url === '/message' && request.method === 'POST') {
-        const body = [];
+console.log(routes.someText);
 
-        request.on("data", chunk => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        return request.on("end", () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1];
-
-            fs.writeFile("message.txt", message, err => {
-                response.statusCode = 302;
-                response.setHeader('Location', '/');
-
-                return response.end();
-            });
-        })
-
-    }
-    response.setHeader('Content-Type', 'text/html');
-    response.write('<html>');
-    response.write('<head><title>My First Page</title></head>');
-    response.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    response.write('</html>');
-    response.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
